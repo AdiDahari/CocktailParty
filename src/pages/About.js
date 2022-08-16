7a033539eafe39ed6883759f69ae5901f9c990c6
Adi Dahari <dahariadi16@gmail.com>
2022-08-17 00:00:18 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">About Us</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure earum
+				modi molestias ipsum mollitia, ullam est velit voluptatum quia, sequi
+				explicabo nisi distinctio tempora nemo cum odit. Mollitia iste, eos
+				distinctio nostrum quo quaerat autem facere explicabo sed iure voluptas.
+			</p>
+		</section>
+	);
+};
+
+export default About;
